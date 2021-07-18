cbd09e6b4a3b31e8fc20b0bf41885c76ed6cff66
gokul1630 <gokulgp163034@gmail.com>
2021-07-18 17:29:40 +0530

    set alt for image carousel

--- src/data/ImageLinks.js
+++ src/data/ImageLinks.js
@@ -1,22 +1,28 @@
 const images = [
   {
     original: 'https://www.sonatech.ac.in/images/banner/abdul-kalam-sona.jpg',
+    originalAlt: 'abdul-kalam-sona.jpg',
   },
   {
     original: 'https://www.sonatech.ac.in/images/tradeshow.jpg',
+    originalAlt: 'images/tradeshow.jpg',
   },
   {
     original: 'https://www.sonatech.ac.in/images/awards.jpg',
+    originalAlt: 'awards.jpg',
   },
   {
     original: 'https://www.sonatech.ac.in/images/ficci-award-2020.jpg',
+    originalAlt: 'ficci-award-2020.jpg',
   },
   {
     original: 'https://www.sonatech.ac.in/images/sona-placement-2021.jpg',
+    originalAlt: 'sona-placement-2021.jpg',
   },
   {
     original:
       'https://www.sonatech.ac.in/images/aicte-viswakarama-award-2020.jpg',
+    originalAlt: 'aicte-viswakarama-award-2020.jpg',
   },
 ];
 
