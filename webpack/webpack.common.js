9a242c7a8486495d89ea64fcd31124e7de3c40ec
gokul1630 <gokulgp3167@gmail.com>
2021-11-24 12:16:52 +0530

    change output directory

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: path.resolve(__dirname, '../src/index.js'),
   output: {
     filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, '../dist'),
+    path: path.resolve(__dirname, '../build'),
     clean: true,
     asyncChunks: true,
   },
